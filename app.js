b026cb28d021268108ea0555722a5c7b820f9c0a
Masanobu Mizutani <bornthiswaymm@gmail.com>
2015-06-14 13:58:48 +0900

    first commit

--- app.js
+++ app.js
@@ -1,24 +1,56 @@
 var express = require('express'),
     path = require('path'),
+    http = require('http'),
     morgan = require('morgan'),
+    parser = require('xml2json'),
     fs = require('fs'),
-    socket = require('socket.io');
-
+    port = 3700;
 
 var app = express();
-
+var url = 'http://localhost:8000/';
 
 app.use(morgan('dev'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','jade');
+app.engine('jade', require('jade').__express);
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/', function(req, res) {
-  res.render('index' ,{});
+
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
 });
 
-var server = app.listen(process.env.PORT || '8080', '0.0.0.0', function() {
-  console.log('App listening at http://%s:%s', server.address().address, server.address().port);
-  console.log("Press Ctrl+C to quit.");
+
+var req = http.get(url, function (res) {
+
+    // テキストファイルの場合は、エンコード指定は重要！
+    var xml = '';
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        xml += chunk;
+        var json = parser.toJson(xml); //returns a string containing the JSON structure by default
+        console.log(json);
+    });
+
+    // ファイルのダウンロードが終わるとendイベントが呼ばれる
+    res.on('end', function () {
+
+    });
+
+});
+
+// app.get('/', function(req, res) {
+//   res.render('index', {song:json.title,
+//   artist: json.artist});
+// });
+
+// 通信エラーなどはここで処理する
+req.on('error', function (err) {
+    console.log('Error: ', err); return;
 });
