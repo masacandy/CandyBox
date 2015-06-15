1676acff95d562677e557b497602359e6cc6dae9
Masanobu Mizutani <bornthiswaymm@gmail.com>
2015-06-15 15:35:26 +0900

    decided to use chat template/save file once but couldnt save correctly

--- app.js
+++ app.js
@@ -2,19 +2,61 @@ var express = require('express'),
     path = require('path'),
     http = require('http'),
     morgan = require('morgan'),
-    parser = require('xml2json'),
+    xml2json = require('xml2json'),
+    io = require('socket.io'),
     fs = require('fs'),
+    serializer = new (require('xmldom')).XMLSerializer,
+    implementation = new (require('xmldom')).DOMImplementation,
+    bodyParser = require('body-parser'),
     port = 3700;
 
 var app = express();
 var url = 'http://localhost:8000/';
 
 app.use(morgan('dev'));
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','jade');
 app.engine('jade', require('jade').__express);
 app.use(express.static(path.join(__dirname,'public')));
+
+var rss;
+
+var document = implementation.createDocument('', '', null);
+document.appendChild(document.createElement('foo'));
+
+var req = http.get(url, function (res) {
+
+    // テキストファイルの場合は、エンコード指定は重要！
+    var xml = '';
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        xml += chunk;
+        console.log(xml);
+        var json = xml2json.toJson(xml); //returns a string containing the JSON structure by default
+        console.log(json);
+
+        fs.writeFile(
+          "./playing.xml",
+          serializer.serializeToString(xml),
+          function(error) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("The file was saved!");
+            }
+          }
+        );
+    });
+    // ファイルのダウンロードが終わるとendイベントが呼ばれる
+    res.on('end', function () {
+    });
+});
+
+
+// 通信エラーなどはここで処理する
+req.on('error', function (err) {
+    console.log('Error: ', err); return;
+});
 
 
 var io = require('socket.io').listen(app.listen(port));
@@ -26,31 +68,8 @@ io.sockets.on('connection', function (socket) {
     });
 });
 
-
-var req = http.get(url, function (res) {
-
-    // テキストファイルの場合は、エンコード指定は重要！
-    var xml = '';
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-        xml += chunk;
-        var json = parser.toJson(xml); //returns a string containing the JSON structure by default
-        console.log(json);
-    });
-
-    // ファイルのダウンロードが終わるとendイベントが呼ばれる
-    res.on('end', function () {
-
-    });
-
+app.get('/', function(req, res) {
+  res.render("index");
 });
 
-// app.get('/', function(req, res) {
-//   res.render('index', {song:json.title,
-//   artist: json.artist});
-// });
-
-// 通信エラーなどはここで処理する
-req.on('error', function (err) {
-    console.log('Error: ', err); return;
-});
+console.log("listening on port" + port);
