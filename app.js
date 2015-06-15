677eecf132883fdfadf4f8e359aea27c548587d7
Masanobu Mizutani <bornthiswaymm@gmail.com>
2015-06-15 20:18:44 +0900

    wanna resolve undefined json status

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ var express = require('express'),
     path = require('path'),
     http = require('http'),
     morgan = require('morgan'),
-    xml2json = require('xml2json'),
+    xml2json = new require('xml2json'),
     io = require('socket.io'),
     bodyParser = require('body-parser'),
     fs = require('fs'),
-    serializer = new (require('xmldom')).XMLSerializer,
-    implementation = new (require('xmldom')).DOMImplementation,
+    playingData = require('./playing.json'),
     port = 3700;
 
 var app = express();
@@ -23,37 +22,19 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var rss;
+console.log(playingData.status.source.title);
 
-var document = implementation.createDocument('', '', null);
-
-var xml = '';
-var currentSong;
 
 function songReq () {
-   http.get(url, function (res) {
-    // テキストファイルの場合は、エンコード指定は重要！
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      var json = xml2json.toJson(chunk);
-      console.log(json);
-      console.log(json.body);
-      if(currentSong != json){
-          currentSong = json;
-          //returns a string containing the JSON structure by default
-          document.appendChild(document.createElement(chunk));
-          fs.writeFile(
-            "./playing.xml",
-            serializer.serializeToString(document),
-            function(error) {
-              if (error) {
-                console.log(error);
-              } else {
-                console.log("The file was saved!");
-              }
-            }
-          );
-        };
+  http.get(url, function (res) {
+  // テキストファイルの場合は、エンコード指定は重要！
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    var json = xml2json.toJson(chunk);
+    console.log(json.status);
+    if(playingData == undefined || playingData != chunk){
+        fs.writeFile('playing.json', json);
+      };
     });
     // ファイルのダウンロードが終わるとendイベントが呼ばれる
     res.on('end', function () {
@@ -63,7 +44,6 @@ function songReq () {
   });
 }
 
-setInterval(songReq, 1000);
 
 
 var io = require('socket.io').listen(app.listen(port));
@@ -71,13 +51,14 @@ var io = require('socket.io').listen(app.listen(port));
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'welcome to the chat'});
 
-    fs.watchFile('./playing.xml', function(curr, prev) {
+    fs.watchFile('./playing.json', function(curr, prev) {
     // on file change we can read the new xml
-      fs.readFile('./playing.xml', function(err, data) {
+      fs.readFile('./playing.json', function(err, data) {
+        console.log("hello");
         if (err) throw err;
         // parsing the new xml data and converting them into json file
-        var json = xml2json.toJson(data);
-        console.log(json);
+        var json = data;
+        console.log("are you here?" + json);
         // send the new data to the client
         socket.volatile.emit('notification', json);
       });
@@ -92,8 +73,6 @@ app.get('/', function(req, res) {
   res.render("index");
 });
 
-setInterval(function() {
-  songReq;
-}, 100);
 
+setInterval(songReq, 1000);
 console.log("listening on port" + port);
