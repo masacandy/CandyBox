4a64d7909254e79685f816ecdaf7678064d7f25c
Masanobu Mizutani <bornthiswaymm@gmail.com>
2015-06-29 12:36:34 +0900

    receive request insted of keeping requesting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     path = require('path'),
     http = require('http'),
+    url = require('url'),
     morgan = require('morgan'),
     xml2json = new require('xml2json'),
     bodyParser = require('body-parser'),
@@ -12,10 +13,6 @@ var server = http.Server(app);
 var io = require('socket.io')(server);
 server.listen(port);
 
-
-
-var url = 'http://localhost:8000/';
-
 app.use(morgan('dev'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','jade');
@@ -26,65 +23,35 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-function songReq () {
-  //アクセスする
-  http.get(url, function (res) {
-    //エンコーディング指定
-    if(err) {
-      console.log("DJ JAMES is not playing");
-    }
-    res.setEncoding('UTF-8');
-    res.on('data', function (chunk) {
-      //ダウンロードしたxmlファイルをjsonに変換
-      var apiJson = xml2json.toJson(chunk);
-      var nowPlaying = JSON.parse(apiJson, 'UTF-8');
-      console.log(nowPlaying);
-      //なぜかstatus以下を持っていない時があったため（もしかしたらコールバックで解決？）
-      if (nowPlaying.hasOwnProperty("status")) {
-        var nowPlayingTitle = nowPlaying.status.source.title;
-        //保存してある曲名を読み込む
-        var playingData = JSON.parse(fs.readFileSync('./playing.json', 'UTF-8'));
-        //保存してあるタイトル
-        var currentTitle = playingData.status.source.title;
-        //apiから来た曲名と、こっちで保存してる曲名が違った場合に上書きする
-        if(currentTitle != nowPlayingTitle){
-            fs.writeFile('playing.json', JSON.stringify(nowPlaying, 'UTF-8'));
-            console.log('saved');
-          } else {
-            console.log("Keep Dancing With This Song");
-          };
-        } else {
-          console.log(nowPlaying);
-          console.log("currently there's no party")
-        }
-      });
-      res.on('end', function () {
-      });
-  }).on('error', function (err) {
-      console.log('Error: ', err); return;
-  });
-}
-
-
 io.sockets.on('connection', function (socket) {
     console.log("connected");
-    //変わったら通知する
-    fs.watchFile('./playing.json', function(curr, prev) {
-      console.log("song chaaaaaaanged");
-      var playingData = JSON.parse(fs.readFileSync('./playing.json', 'utf8'));
-      socket.volatile.emit('notification', {'song':playingData.status.source.title,
-                                            'artist':playingData.status.source.artist});
-    });
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
-    });
+
+    //var playingData = JSON.parse(fs.readFileSync('./playing.json', 'UTF-8'));
+
+    //io.sockets.emit('send', {'song':playingData.song, 'artist':playingData.artist});
+
 });
+
+
+io.sockets.on('save', function(data) {
+  console.log("insideif");
+  //fs.writeFile('playing.json', JSON.stringify(data, 'UTF-8'));
+});
+
+
 
 app.get('/', function(req, res) {
   res.render("index");
 });
 
+app.post('/songinfo', function(req, res) {
+  var data = req.body;
+  console.log("insideif");
 
-setInterval(songReq, 100);
+  io.sockets.emit('save', data);
+  console.log(data.song);
+  io.sockets.emit('send', {'song':data.song,
+                                      'artist':data.artist});
+});
+
 console.log("listening on port" + port);
